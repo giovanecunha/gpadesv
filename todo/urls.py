c686c7fadd11b917f4667cd70d4a4a2869fa38bb
giovanecunha <giovanescunha@gmail.com>
2018-10-16 22:48:26 -0300

    bootstrap

--- todo/urls.py
+++ todo/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url
+from todo.core import views
 
 urlpatterns = [
+    url(r'', views.home),
     path('admin/', admin.site.urls),
 ]
