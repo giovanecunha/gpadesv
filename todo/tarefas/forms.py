6cbc42e01a939a52694e8e324ecb259494e33501
giovanecunha <giovanescunha@gmail.com>
2018-11-05 21:07:40 -0200

    bootstrag para categoria

--- todo/tarefas/forms.py
+++ todo/tarefas/forms.py
@@ -3,6 +3,8 @@ from django import forms
 from .models import Categoria, Tarefa
 
 class CategoriaForm(forms.ModelForm):
+    nome = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
+    descricao = forms.CharField(widget=forms.Textarea(attrs={'class':'form-control'}))
     class Meta:
         model = Categoria
         fields = '__all__' #mostra todos os campos
