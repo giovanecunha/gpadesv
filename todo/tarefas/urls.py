72ec732b898dce407d4fb4674c1596beefdce9ae
giovanecunha <giovanescunha@gmail.com>
2018-11-12 21:25:00 -0200

    commit

--- todo/tarefas/urls.py
+++ todo/tarefas/urls.py
@@ -6,4 +6,5 @@ app_name = 'myapp'
 
 urlpatterns = [
     url(r'^nova-categoria/$', views.nova_categoria, name='nova_categoria'),
+    url(r'^nova-tarefa/$', views.nova_tarefa, name='nova_tarefa'),
 ]
